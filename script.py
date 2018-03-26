c30383d3b3aaa37243b8159616e792a0292ac252
Rohan Chopra <09rohanchopra@gmail.com>
2018-03-26 20:30:57 +0530

    Save required CSV data to redis as json

--- script.py
+++ script.py
@@ -3,6 +3,8 @@ from bs4 import BeautifulSoup
 import wget
 import zipfile
 import os
+import redis
+import pandas as pd
 
 url = 'https://www.bseindia.com/markets/equity/EQReports/Equitydebcopy.aspx'
 
@@ -23,6 +25,16 @@ zip_ref.extractall("csv-files/")
 csv_file = zip_ref.namelist()[0]
 zip_ref.close()
 
-print(csv_file)
+print(csv_file[0:-4])
 
 os.remove(zip_name)
+
+df = pd.read_csv("csv-files/"+csv_file)
+
+df = df[['SC_CODE', 'SC_NAME', 'OPEN', 'HIGH', 'LOW', 'CLOSE']].copy()
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+r.set("csv_file[0:-4]", df.to_msgpack(compress='zlib'))
+print(pd.read_msgpack(r.get("csv_file[0:-4]")).head())
+
+
