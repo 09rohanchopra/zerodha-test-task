f5a3a7e2271a8c62de20b028136706aeecf1de62
Rohan Chopra <09rohanchopra@gmail.com>
2018-03-27 01:52:22 +0530

    Calculate top gainers and loosers and save to redis

--- script.py
+++ script.py
@@ -34,8 +34,26 @@ df = pd.read_csv("csv-files/"+csv_file)
 df = df[['SC_CODE', 'SC_NAME', 'OPEN', 'HIGH', 'LOW', 'CLOSE']].copy()
 r = redis.Redis(host='localhost', port=6379, db=0)
 
+df_compute = df.copy()
+df_compute['PERCENTAGE'] = (df['CLOSE'] - df['OPEN'])/ df['OPEN']
+
+
+df_gain = df_compute.nlargest(10,['PERCENTAGE']).copy()
+df_loose = df_compute.nsmallest(10,['PERCENTAGE']).copy()
+
+
 for index, row in df.iterrows():
 	r.hmset(row['SC_CODE'],row.to_dict())
-	r.set(row['SC_NAME'],row['SC_CODE'])
+	r.set("equity:"+row['SC_NAME'],row['SC_CODE'])
 
+for key in r.scan_iter("gain:*"):
+	r.delete(key)
+for key in r.scan_iter("loose:*"):
+	r.delete(key)
+
+for index, row in df_gain.iterrows():
+	r.set("gain:"+row['SC_NAME'],row['SC_CODE'])
+
+for index, row in df_loose.iterrows():
+	r.set("loose:"+row['SC_NAME'],row['SC_CODE'])
 
